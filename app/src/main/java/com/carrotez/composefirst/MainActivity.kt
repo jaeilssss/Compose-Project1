a37c0b4314d47c3fc8d62a49b96eb42191ce1c57
jaeilssss <jaeil95ssss@gmail.com>
2023-03-19 11:51:32 +0900

    [feat] TopHeader 구현

--- app/src/main/java/com/carrotez/composefirst/MainActivity.kt
+++ app/src/main/java/com/carrotez/composefirst/MainActivity.kt
@@ -3,11 +3,21 @@ package com.carrotez.composefirst
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.CornerSize
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.carrotez.composefirst.ui.theme.ComposeFirstTheme
 
 class MainActivity : ComponentActivity() {
@@ -16,8 +26,8 @@ class MainActivity : ComponentActivity() {
         setContent {
             ComposeFirstTheme {
                 // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
-                    Greeting("test 입니다!!!!")
+                MyApp{
+                        TopHeader()
                 }
             }
         }
@@ -25,14 +35,56 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "test $name!")
+fun MyApp(content : @Composable () -> Unit){
+    
+    Surface(color = MaterialTheme.colors.background) {
+
+        content()
+    }
 }
+
+@Preview
+@Composable
+fun TopHeader(totalPerPerson : Double = 134.0){
+
+    Surface(modifier = Modifier
+        .fillMaxWidth()
+        .height(150.dp)
+        .clip(shape = CircleShape.copy(all = CornerSize(12.dp))),
+        color = Color(0xFFE9D7F7)
+    ) {
+        //.clip(shape = RoundedCornerShape(coner = ConerSize(12.dp)))
+        Column(
+            modifier = Modifier.padding(12.dp),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+
+            val total = "%.2f".format(totalPerPerson)
+
+            Text(text = "Total Per Person",
+                style = MaterialTheme.typography.h5)
+
+
+            Text(text = "$$total",
+                style = MaterialTheme.typography.h4,
+            fontWeight = FontWeight.ExtraBold)
+
+        }
+    }
+
+}
+
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     ComposeFirstTheme {
-        Greeting("Android")
+
+        MyApp{
+            Text(text = "Hello Again")
+        }
+
+
     }
 }
