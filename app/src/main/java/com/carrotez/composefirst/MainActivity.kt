e483ab82420504b3dac4e50308889d1d6ff07959
jaeilssss <jaeil95ssss@gmail.com>
2023-03-19 14:07:54 +0900

    [feat] component.kt 구현

--- app/src/main/java/com/carrotez/composefirst/MainActivity.kt
+++ app/src/main/java/com/carrotez/composefirst/MainActivity.kt
@@ -3,6 +3,7 @@ package com.carrotez.composefirst
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.CornerSize
@@ -75,6 +76,28 @@ fun TopHeader(totalPerPerson : Double = 134.0){
 
 }
 
+
+@Preview
+@Composable
+fun MainContent(){
+
+    Surface(modifier = Modifier
+        .padding(2.dp)
+        .fillMaxWidth(),
+        shape = RoundedCornerShape(corner = CornerSize(8.dp)),
+        border = BorderStroke(width = 1.dp , color = Color.LightGray)
+    ) {
+
+        Column() {
+
+            Text(text ="Hello Again")
+            Text(text ="Hello Again")
+            Text(text ="Hello Again")
+
+        }
+
+    }
+}
 
 @Preview(showBackground = true)
 @Composable
